b63f02bf7c55e6f62c64fc710bcacc92a9b48fcc
Jasper Q <jasperisqiao@gmail.com>
2025-12-27 18:44:44 -0800

    wrap flask in azure functions

--- function_app.py
+++ function_app.py
@@ -1,25 +1,8 @@
 import azure.functions as func
 import logging
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+# Import the Flask app from first.py
+from first import app as flask_app
 
-@app.route(route="http_trigger")
-def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This is my chess application.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+# Wrap the Flask app with WsgiFunctionApp
+app = func.WsgiFunctionApp(app=flask_app.wsgi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
